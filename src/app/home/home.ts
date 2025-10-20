649c21917a5660f870c53247bf4ca6b8f02d6bb5
PavelChukharev <pchukharev@kapitus.com>
2025-10-20 20:07:59 +0400

    init

---
+++ src/app/home/home.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-home',
+  template: ` <p>Hello world</p> `,
+})
+export default class HomeComponent {
+}
